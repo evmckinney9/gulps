fix: monodromy to makhlin conversion issues for local equiv sign

src/gulps/linear_program.py
@@ -12,6 +12,8 @@ from scipy.optimize import linprog
 from gulps.utils.invariants import LEN_GATE_INVARIANTS, GateInvariants
 from gulps.utils.qlr import len_qlr, qlr_inequalities
 
+id_inv = GateInvariants(logspec=(0.0, 0.0, 0.0, 0.0))
+
 
 class MinimalOrderedISAConstraints:
     """Minimal LP constraints for ordered, defined ISA sequences."""
@@ -60,8 +62,8 @@ class MinimalOrderedISAConstraints:
     def set_target(self, target_gate: GateInvariants):
         # NOTE avoid reconstructing all of b_ub by tracking the last set target gate
         # when setting a new target, remove the contribution of the previous target
-        self._target_def = target_gate.monodromy
-        ct = np.dot(self._ciplus1_block, self._target_def)
+        self._target_def = target_gate
+        ct = np.dot(self._ciplus1_block, self._target_def.monodromy)
         self.b_ub[-len_qlr:] += self.last_iter_ct - ct
         self.last_iter_ct = ct
 
@@ -69,12 +71,12 @@ class MinimalOrderedISAConstraints:
         # edge case, if there were no free variables in x_vec
         if len(self.A_ub[0]) == 0:
             if np.all(0 <= self.b_ub):
-                blocks = (
-                    np.zeros(LEN_GATE_INVARIANTS),
-                    self.isa_sequence[0].monodromy,
+                intermediate_invariants = (
+                    id_inv,
+                    self.isa_sequence[0],
                     self._target_def,
                 )
-                return self._extract_from_blocks(blocks)
+                return self.isa_sequence, intermediate_invariants
             else:
                 return None, None
         result = linprog(
@@ -85,19 +87,21 @@ class MinimalOrderedISAConstraints:
             options={"disp": log_output},
         )
         if result.success:
-            blocks = (
-                np.zeros(LEN_GATE_INVARIANTS),
-                self.isa_sequence[0].monodromy,
-                result.x,
-                self._target_def,
-            )
-            return self._extract_from_blocks(blocks)
+            return self._extract_from_blocks(result.x)
         return None, None
 
-    def _extract_from_blocks(self, blocks):
-        full_c_vec = np.concatenate(blocks)
-        intermediate_invariants = [
-            GateInvariants(tuple(full_c_vec[i : i + LEN_GATE_INVARIANTS]))
-            for i in range(0, len(full_c_vec), LEN_GATE_INVARIANTS)
+    def _extract_from_blocks(self, lp_vec):
+        lp_invariants = [
+            GateInvariants(tuple(lp_vec[i : i + LEN_GATE_INVARIANTS]))
+            for i in range(0, len(lp_vec), LEN_GATE_INVARIANTS)
         ]
+
+        # Build full intermediate sequence
+        intermediate_invariants = (
+            id_inv,
+            self.isa_sequence[0],
+            *lp_invariants,
+            self._target_def,
+        )
+
         return self.isa_sequence, intermediate_invariants

src/gulps/synthesis_plugin.py
@@ -71,6 +71,7 @@ class GulpsSynthesisPlugin(UnitarySynthesisPlugin):
         return None
 
     def run(self, unitary: np.ndarray, **options) -> DAGCircuit:
+        print("GulpsSynthesisPlugin.run called with options:", options)
         # basis_gates = options.get("basis_gates", None)
         target = options.get("target")
         if target is None:
@@ -86,7 +87,7 @@ class GulpsSynthesisPlugin(UnitarySynthesisPlugin):
 
         if (
             GulpsSynthesisPlugin._decomposer is None
-            or GulpsSynthesisPlugin._decomposer.gate_set != gate_set
+            or GulpsSynthesisPlugin._decomposer.isa.gate_set != gate_set
         ):
             GulpsSynthesisPlugin._decomposer = GulpsDecomposer(
                 gate_set=gate_set,

src/gulps/utils/invariants.py
@@ -7,6 +7,7 @@ import numpy as np
 from monodromy.coordinates import positive_canonical_to_monodromy_coordinate
 from qiskit._accelerate.two_qubit_decompose import weyl_coordinates
 from qiskit.circuit import Gate
+from qiskit.circuit.library import UnitaryGate
 from qiskit.quantum_info import Operator
 from qiskit.synthesis.two_qubit import TwoQubitWeylDecomposition
 from weylchamber import canonical_gate
@@ -39,6 +40,8 @@ class GateInvariants:
         cls, gate: Gate | np.ndarray, name: Optional[str] = None
     ) -> "GateInvariants":
         U = Operator(gate).data
+        if not isinstance(gate, Gate):
+            gate = UnitaryGate(gate, label=name)
         coords = cls._unitary_to_mono_coordinates(U)
         return cls(logspec=coords, name=name, unitary=gate)
 
@@ -88,17 +91,23 @@ class GateInvariants:
 
         Matches the normalized convention from weylchamber.g1g2g3
         """
-        if self._makhlin is None:
-            weyl = np.pi * self.weyl / 2  # Normalize back for Makhlin formula
-            g0 = np.prod(np.cos(2 * weyl) ** 2) - np.prod(np.sin(2 * weyl) ** 2)
-            g1 = np.prod(np.sin(4 * weyl)) / 4
-            g2 = (
-                4 * np.prod(np.cos(2 * weyl) ** 2)
-                - 4 * np.prod(np.sin(2 * weyl) ** 2)
-                - np.prod(np.cos(4 * weyl))
-            )
-            self._makhlin = np.array([g0, g1, g2], dtype=np.double)
-        return self._makhlin
+        from qiskit._accelerate.two_qubit_decompose import (
+            two_qubit_local_invariants as tqli_rs,
+        )
+
+        return tqli_rs(Operator(self.unitary).data)
+
+        # if self._makhlin is None:
+        #     weyl = np.pi * self.weyl / 2  # Normalize back for Makhlin formula
+        #     g0 = np.prod(np.cos(2 * weyl) ** 2) - np.prod(np.sin(2 * weyl) ** 2)
+        #     g1 = np.prod(np.sin(4 * weyl)) / 4
+        #     g2 = (
+        #         4 * np.prod(np.cos(2 * weyl) ** 2)
+        #         - 4 * np.prod(np.sin(2 * weyl) ** 2)
+        #         - np.prod(np.cos(4 * weyl))
+        #     )
+        #     self._makhlin = np.array([g0, g1, g2], dtype=np.double)
+        # return self._makhlin
 
     @property
     def canonical_matrix(self) -> np.ndarray:
@@ -142,11 +151,14 @@ def recover_local_equivalence(U_target, U_basis):
 
     local_coords1 = np.array([target_decomp.a, target_decomp.b, target_decomp.c])
     local_coords2 = np.array([basis_decomp.a, basis_decomp.b, basis_decomp.c])
-    if not np.allclose(np.abs(local_coords1 - local_coords2), 0, atol=1e-4):
+    if not np.allclose(np.abs(local_coords1 - local_coords2), 0, atol=1e-3):
         if not np.isclose(np.abs(target_decomp.c), np.abs(basis_decomp.c)):
             raise ValueError(
                 f"Gates are not locally equivalent. Difference: {np.abs(local_coords1 - local_coords2)}"
             )
+        print(
+            "Warning: Possible sign difference in c parameter during local equivalence recovery."
+        )
         warnings.warn(
             "Possible sign difference in c parameter during local equivalence recovery."
         )