83de8b9b7efec35673519e84076780fb651992a5
Evan McKinney <evmckinney9@gmail.com>
2025-07-16 16:15:46 -0400

    remove old version files from docs/

--- docs/deprecate/hetero_isas/monodromy_lp/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Two-qubit decomposition using monodromy linear programming."""
-
-from .decomposer import MonodromyLPDecomposer, MonodromyLPDecomposerResult
-from .isa import MonodromyLPGate, MonodromyLPISA
