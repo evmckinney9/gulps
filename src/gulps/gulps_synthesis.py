wip reflect entire sequence to fix c sign

src/gulps/gulps_synthesis.py
@@ -67,24 +67,9 @@ class GulpsDecomposer:
         sentence_out, intermediates = constraints.solve(log_output=log_output)
         return sentence_out, intermediates
 
-    def _run(
-        self,
-        target: Union[np.ndarray, Gate, GateInvariants],
-        return_dag: bool = False,
-        log_output: bool = False,
-    ) -> QuantumCircuit | DAGCircuit:
-        target_inv = (
-            target
-            if isinstance(target, GateInvariants)
-            else GateInvariants.from_unitary(target)
-        )
-
-        # TODO
-        self._eval_edge_case(target_inv)
-
-        sentence_out = None
-        intermediates = None
-
+    def _best_decomposition(
+        self, target_inv: GateInvariants, log_output: bool = False
+    ) -> tuple[List[GateInvariants], List[GateInvariants]]:
         if self.isa._precompute_polytopes:
             sentence, rho_bool = self.isa.polytope_lookup(target_inv)
 
@@ -110,9 +95,31 @@ class GulpsDecomposer:
                 raise RuntimeError("No valid ISA sentence found via LP enumeration.")
 
         # FIXME
-        useful_intermediates = intermediates[1:-1]  # Skip identity, and target
-        useful_intermediates += (target_inv,)  # Append target as last intermediate
+        useful_intermediates = intermediates[1:]  # Skip identity, and target
+        # useful_intermediates += (target_inv,)  # Append target as last intermediate
+        return sentence_out, useful_intermediates
 
+    def _run(
+        self,
+        target: Union[np.ndarray, Gate, GateInvariants],
+        return_dag: bool = False,
+        log_output: bool = False,
+    ) -> QuantumCircuit | DAGCircuit:
+        # Convert target to GateInvariants if necessary
+        target_inv = (
+            target
+            if isinstance(target, GateInvariants)
+            else GateInvariants.from_unitary(target)
+        )
+
+        # TODO
+        self._eval_edge_case(target_inv)
+
+        # Find the best decomposition using LP
+        sentence_out, useful_intermediates = self._best_decomposition(
+            target_inv, log_output=log_output
+        )
+        # Convert the sentence to a circuit or DAG
         return self._numerics.run(
             sentence_out,
             useful_intermediates,

src/gulps/linear_program.py
@@ -63,10 +63,13 @@ class MinimalOrderedISAConstraints:
         # NOTE avoid reconstructing all of b_ub by tracking the last set target gate
         # when setting a new target, remove the contribution of the previous target
         self._target_def = target_gate
+        # FIXME, actual_target is for debugging, remove later
         if rho_reflect:
             target_monodromy = target_gate.rho_reflect
+            self._actual_target = target_gate
         else:
             target_monodromy = target_gate.monodromy
+            self._actual_target = GateInvariants(logspec=target_monodromy)  # XXX FIXME
         ct = np.dot(self._ciplus1_block, target_monodromy)
         self.b_ub[-len_qlr:] += self.last_iter_ct - ct
         self.last_iter_ct = ct
@@ -78,7 +81,7 @@ class MinimalOrderedISAConstraints:
                 intermediate_invariants = (
                     id_inv,
                     self.isa_sequence[0],
-                    self._target_def,
+                    self._actual_target,  # self._target_def, # FIXME
                 )
                 return self.isa_sequence, intermediate_invariants
             else:
@@ -111,7 +114,7 @@ class MinimalOrderedISAConstraints:
             id_inv,
             self.isa_sequence[0],
             *lp_invariants,
-            self._target_def,
+            self._actual_target,  # self._target_def, #FIXME
         )
 
         return self.isa_sequence, intermediate_invariants